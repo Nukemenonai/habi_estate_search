bfbe8e5334ffde6694a6732409139d0514ec315b
Nukemenonai <david.giovanni.ovalle@gmail.com>
2021-12-06 10:26:29 -0500

    update to base query

--- queries/estate_queries.py
+++ queries/estate_queries.py
@@ -1,12 +1,19 @@
+"""estate queries"""
+
 GET_ESTATE = """
 SELECT 
     address,
     city, 
     price, 
-    description, year,
-    property_id,
-    status_id,
-    update_date
+    CASE description
+        WHEN '' THEN 'sin descripción'
+        ELSE description
+    END AS descripcion,
+    CASE status_id
+        WHEN 3 THEN 'pre_venta'
+        WHEN 4 THEN 'en_venta'
+        WHEN 5 THEN 'vendido'
+    END AS estado
 FROM `habi_db`.`property` as p
 INNER JOIN `habi_db`.`status_history` as s
 ON p.id = s.property_id
