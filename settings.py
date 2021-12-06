4b57a4eb3eeda0dd31e4ecc7ab6ad02ec4423238
Nukemenonai <david.giovanni.ovalle@gmail.com>
2021-12-06 07:06:04 -0500

    update: settings file

---
+++ settings.py
@@ -0,0 +1,10 @@
+"""General settings"""
+
+APP_NAME = "Estate Search"
+CONN_NAME = {
+    "host": "3.130.126.210",
+    "port": 3309,
+    "user": "pruebas",
+    "password": "VGbt3Day5R",
+    "database": "habi_db",
+}
