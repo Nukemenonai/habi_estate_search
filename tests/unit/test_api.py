35a3c294c56bec4cb9b3166e49f357765483eda5
Nukemenonai <david.giovanni.ovalle@gmail.com>
2021-12-06 07:19:23 -0500

    update: separation of tests by method

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -2,24 +2,20 @@ import requests
 import unittest
 import json
 
+
 class TestApi(unittest.TestCase):
-    
     def test_get_status(self):
         expected_result = {"status": "up and running!"}
 
         r = requests.get("http://localhost:8000/status")
         self.assertEqual(r.status_code, 200)
         self.assertEqual(r.json(), expected_result)
-        data = {'some': 'value'}
+    def test_post(self):
+        data = {"some": "value"}
         expected_result = {"posted": "ok"}
         r = requests.post("http://localhost:8000/estate", json=json.dumps(data))
         self.assertEqual(r.status_code, 200)
-        #self.assertEqual(expected_result, r.json())
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
-
-
-
